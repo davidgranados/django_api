4204658da0c8d96d6065c7f83e566a1731c78eef
davidgranados-celeritech <david.granados@celeritech.biz>
2022-01-23 11:06:55 -0500

    03 - Changing the JWT payload content and extend the expiration time - Django REST Framework

---
+++ accounts/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token["first_name"] = user.first_name
+        token["last_name"] = user.last_name
+        token["email"] = user.email
+        token["user_id"] = user.pk
+
+        return token
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from accounts import serializers
+
+
+class LoginView(TokenObtainPairView):
+    serializer_class = serializers.CustomTokenObtainPairSerializer
