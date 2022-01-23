23424dbaa5231b343b4ffbab66998a5bcd4179d9
davidgranados-celeritech <david.granados@celeritech.biz>
2022-01-23 10:50:15 -0500

    02 - Django rest framework JWT AUTH - what are migrations and why - override the default user model

---
+++ accounts/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from accounts.managers import CustomUserManager
+
+
+class User(AbstractUser):
+    """
+    Custom user model that support using email instead of username.
+    """
+    username = None
+    email = models.EmailField(_("email address"), max_length=255, unique=True, blank=False, null=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
